9e3f0b4109587c232df40007d151c8ce0ee2d875
Ilya Lesokhin <ilyal@mellanox.com>
2016-09-22 11:58:25 +0300

    Move resync operation back to driver
    Change-Id: Ie7f46b2028df69fa9011cec1eed22e8403400bdc

--- af_ktls_offload.c
+++ af_ktls_offload.c
@@ -16,11 +16,14 @@ EXPORT_SYMBOL(tls_destroy_record);
 
 void clean_offloaded_data(struct sock *sk, int closing_sk)
 {
+	struct ktls_offload_context *context = sk->sk_tls_offload;
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct tls_record_info *info, *temp;
 	u32 end_seq;
+	unsigned long flags;
 
-	list_for_each_entry_safe(info, temp, &tp->offload_list, list) {
+	spin_lock_irqsave(&context->lock, flags);
+	list_for_each_entry_safe(info, temp, &context->records_list, list) {
 		end_seq = info->start_seq + info->len;
 		if (before(tp->snd_una, end_seq) && !closing_sk)
 			break;
@@ -30,106 +33,28 @@ void clean_offloaded_data(struct sock *sk, int closing_sk)
 
 		tls_destroy_record(info);
 	}
+	spin_unlock_irqrestore(&context->lock, flags);
 }
 
-static void send_sync_skb(struct sock *sk,
-			  struct tls_record_info *info,
-			  struct sk_buff *skb, u32 seq)
-{
-	const struct inet_connection_sock *icsk = inet_csk(sk);
-	struct inet_sock *inet = inet_sk(sk);
-	int headln = skb_transport_offset(skb) + tcp_hdrlen(skb);
-	int max_header = sk->sk_prot->max_header;
-	struct sk_buff *nskb = alloc_skb(max_header, GFP_ATOMIC);
-	int sync_size = 0;
-	int mss;
-
-	if (!nskb)
-		return;
-
-	nskb->sk = skb->sk;
-	skb_reserve(nskb, max_header);
-
-	memcpy(skb_push(nskb, headln), skb->data, headln);
-	skb_reset_transport_header(nskb);
-
-	skb_shinfo(nskb)->gso_size = 0;
-	skb_shinfo(nskb)->gso_type = skb_shinfo(skb)->gso_type;
-
-	sync_size = seq - info->start_seq;
-	if (sync_size) {
-		int i;
-
-		if (sync_size >= 1 << 16) {
-			pr_err("bad packet %u %u\n", seq, info->start_seq);
-			return;
-		}
-
-		tcp_hdr(nskb)->seq = htonl(info->start_seq);
-
-		mss = tcp_current_mss(skb->sk);
-		if (sync_size > mss) {
-			skb_shinfo(nskb)->gso_size = mss;
-			skb_shinfo(nskb)->gso_segs = DIV_ROUND_UP(sync_size,
-								  mss);
-		}
-
-		nskb->len += sync_size;
-		nskb->data_len += sync_size;
-		i = 0;
-		while (sync_size > 0) {
-			skb_shinfo(nskb)->frags[i] = info->frags[i];
-			skb_frag_ref(nskb, i);
-			sync_size -= skb_frag_size(
-					&skb_shinfo(nskb)->frags[i]);
-
-			if (sync_size < 0) {
-				skb_frag_size_add(
-					&skb_shinfo(nskb)->frags[i],
-					sync_size);
-			}
-
-			i++;
-		}
-		skb_shinfo(nskb)->nr_frags = i;
-	}
-
-	nskb->ooo_okay = skb->ooo_okay;
-	skb->ooo_okay = 0;
-
-	nskb->ip_summed = skb->ip_summed;
-	icsk->icsk_af_ops->send_check(sk, nskb);
-
-	nskb->sync_skb = 1;
-	pr_info("sending sync skb seq [%u-%u)\n",
-		info->start_seq, info->start_seq + nskb->data_len);
-	icsk->icsk_af_ops->queue_xmit(sk, nskb, &inet->cork.fl);
-}
-
-static void tcp_check_sync(struct sock *sk, struct sk_buff *skb,
-			   u32 seq, u32 end_seq) {
+struct tls_record_info *ktls_get_record(
+			struct ktls_offload_context *context,
+			u32 seq) {
 	struct tls_record_info *info;
-	struct tcp_sock *tp = tcp_sk(sk);
 
-	pr_info("tcp_check_sync %u %u\n", tp->expected_seq, seq);
-
-	if (tp->expected_seq == seq || list_empty(&tp->offload_list))
-		goto update_expected;
-
-	list_for_each_entry(info, &tp->offload_list, list) {
+	list_for_each_entry(info, &context->records_list, list) {
 		u32 end_seq = info->start_seq + info->len;
 
 		if (before(seq, end_seq))
-			break;
+			return info;
 	}
 
-	send_sync_skb(sk, info, skb, seq);
-update_expected:
-	tp->expected_seq = end_seq;
+	return NULL;
 }
+EXPORT_SYMBOL(ktls_get_record);
 
 int tls_send_record(struct sock *sk, struct tls_record_info *record)
 {
+	struct ktls_offload_context *context = sk->sk_tls_offload;
 	struct tcp_sock *tp = tcp_sk(sk);
 	int i = 0;
 	skb_frag_t *frag;
@@ -138,7 +63,10 @@ int tls_send_record(struct sock *sk, struct tls_record_info *record)
 
 	lock_sock(sk);
 	record->start_seq = tp->write_seq;
-	list_add_tail(&record->list, &tp->offload_list);
+
+	spin_lock_irq(&context->lock);
+	list_add_tail(&record->list, &context->records_list);
+	spin_unlock_irq(&context->lock);
 
 	while (flags == MSG_SENDPAGE_NOTLAST) {
 		frag = &record->frags[i];
@@ -164,6 +92,6 @@ EXPORT_SYMBOL(tls_send_record);
 
 struct tcp_offload_ops tls_offload_ops = {
 	clean_offloaded_data,
-	tcp_check_sync
+	NULL
 };
 EXPORT_SYMBOL(tls_offload_ops);
--- af_ktls_offload.h
+++ af_ktls_offload.h
@@ -3,6 +3,8 @@
 
 #include <linux/types.h>
 
+#include "af_ktls.h"
+
 struct tls_record_info {
 	struct list_head list;
 	u32 start_seq;
@@ -11,6 +13,30 @@ struct tls_record_info {
 	skb_frag_t	frags[MAX_SKB_FRAGS];
 };
 
+struct ktls_key {
+	char key[KTLS_AES_GCM_128_KEY_SIZE];
+	char salt[KTLS_AES_GCM_128_SALT_SIZE];
+	char iv[KTLS_AES_GCM_128_IV_SIZE];
+};
+
+struct ktls_keys {
+	struct ktls_key tx;
+	struct ktls_key rx;
+};
+
 extern struct tcp_offload_ops tls_offload_ops;
+
+struct ktls_offload_context {
+	struct list_head records_list;
+	u32 expectedSN;
+	spinlock_t lock; /* protects records list */
+};
+
+void tls_destroy_record(struct tls_record_info *record);
+int tls_send_record(struct sock *sk, struct tls_record_info *record);
+
+struct tls_record_info *ktls_get_record(
+			struct ktls_offload_context *context,
+			u32 seq);
 
 #endif /* AF_KTLS_OFFLOAD_H_ */
